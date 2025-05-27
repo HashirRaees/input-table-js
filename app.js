2d6cc3e7080dee50ed90c6fe1a2548ae318ed4ff
HashirRaees <hashirrais13@gmail.com>
2025-05-27 21:32:28 +0500

    changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let number = prompt("Enter a number")
 
-for(let table = 1; table <= 10; table++){
-document.write(table+"x"+number+"="+(table*number)+"<br>");  
+for (let i = 1; i <= 10; i++) {
+    document.write(i + "x" + number + "=" + (i * number));
 } 
